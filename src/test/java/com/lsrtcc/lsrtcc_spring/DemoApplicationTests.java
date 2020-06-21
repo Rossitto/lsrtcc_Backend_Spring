d0d89535628c47916f5febb2845fc2f54baaa6d0
Rossitto <lucas74rossi@gmail.com>
2020-06-21 20:54:26 -0300

    first commit!

---
+++ src/test/java/com/lsrtcc/lsrtcc_spring/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lsrtcc.lsrtcc_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
