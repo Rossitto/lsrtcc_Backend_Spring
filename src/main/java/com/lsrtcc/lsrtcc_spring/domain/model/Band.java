b9f98a00a693683ae78b7e7d1ce9d3e2171bc536
Rossitto <lucas74rossi@gmail.com>
2020-07-29 21:23:00 -0300

    created Band.java domain model

---
+++ src/main/java/com/lsrtcc/lsrtcc_spring/domain/model/Band.java
@@ -0,0 +1,135 @@
+package com.lsrtcc.lsrtcc_spring.domain.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import org.springframework.format.annotation.NumberFormat;
+
+@Entity
+public class Band {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Size(max = 30)
+    private String name;
+
+    @Size(max = 22)
+    private String cnpj;
+
+    // TODO: incluir @NumberFormat ?
+    @Size(max = 10)
+    private double fee_brl;
+
+    @NumberFormat
+    private int members_qtd;
+
+    @Size(max = 50)
+    private String style; // maybe make it a ENUM someday
+
+    @NotBlank
+    @Size(max = 20)
+    private String phone;
+
+    @NotBlank
+    @Email
+    @Size(max = 100)
+    private String email;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public double getFee_brl() {
+        return fee_brl;
+    }
+
+    public void setFee_brl(double fee_brl) {
+        this.fee_brl = fee_brl;
+    }
+
+    public int getMembers_qtd() {
+        return members_qtd;
+    }
+
+    public void setMembers_qtd(int members_qtd) {
+        this.members_qtd = members_qtd;
+    }
+
+    public String getStyle() {
+        return style;
+    }
+
+    public void setStyle(String style) {
+        this.style = style;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Band other = (Band) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+
+}
