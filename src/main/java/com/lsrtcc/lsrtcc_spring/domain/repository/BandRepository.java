bceac465ae648743b1010493ef8ad4089a12e185
Rossitto <lucas74rossi@gmail.com>
2020-08-01 11:27:39 -0300

    Added class diagram and insomnia file (API calls)

--- src/main/java/com/lsrtcc/lsrtcc_spring/domain/repository/BandRepository.java
+++ src/main/java/com/lsrtcc/lsrtcc_spring/domain/repository/BandRepository.java
@@ -1,12 +1,11 @@
 package com.lsrtcc.lsrtcc_spring.domain.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository;
-
 import java.util.List;
 
 import com.lsrtcc.lsrtcc_spring.domain.model.Band;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 @Repository
 public interface BandRepository extends JpaRepository<Band, Long> {
@@ -17,12 +16,11 @@ public interface BandRepository extends JpaRepository<Band, Long> {
 
     List<Band> findByNameContaining(String name);
 
-    List<Band> findAll();
-
     // TODO: test if method below is working: findByStyleContaining
     List<Band> findByStyleContaining(String style);
 
-    // TODO: test if method below is working: findAllByAttribute
-    @Query("select band from #{#entityName} band where band.attribute = ?1")
-    List<Band> findAllByAttribute(String style);
+    // // TODO: this method were causeing the application to failt. So, I commented
+    // it (01/08/2020)
+    // @Query("select band from #{#entityName} band where band.attribute = ?1")
+    // List<Band> findAllByAttribute(String style);
 }
