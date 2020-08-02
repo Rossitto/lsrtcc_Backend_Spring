e686a87f9c873953986d8702d97a781c97215658
Rossitto <lucas74rossi@gmail.com>
2020-08-02 17:58:06 -0300

    changed fee_brl from decimal(7,2) to decimal(8,2)

--- src/main/resources/sql/create_table_band.sql
+++ src/main/resources/sql/create_table_band.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS band (
     id BIGINT AUTO_INCREMENT,
     name varchar(30),
     cnpj varchar(22),
-    fee_brl decimal(7, 2),
+    fee_brl decimal(8, 2),
     members_num smallint,
     style varchar(50),
     phone varchar(20),
