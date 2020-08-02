be410c6135aeb0399e6e2cb35b8ecaf3e7c89509
Rossitto <lucas74rossi@gmail.com>
2020-08-02 15:55:00 -0300

    added @Bean in main file to force UTF-8 encoding

--- src/main/java/com/lsrtcc/lsrtcc_spring/DemoApplication.java
+++ src/main/java/com/lsrtcc/lsrtcc_spring/DemoApplication.java
@@ -1,13 +1,32 @@
 package com.lsrtcc.lsrtcc_spring;
 
+import javax.servlet.Filter;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import org.springframework.web.filter.CharacterEncodingFilter;
 
 @SpringBootApplication
+@EnableSpringDataWebSupport
+@EnableCaching
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	@Bean
+	public Filter getCharacterEncodingFilter() {
+
+		CharacterEncodingFilter encodingFilter = new CharacterEncodingFilter();
+
+		encodingFilter.setEncoding("UTF-8");
+		encodingFilter.setForceEncoding(true);
+
+		return encodingFilter;
+
+	}
 }
